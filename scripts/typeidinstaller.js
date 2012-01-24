7f9a64b1c1cf3175c1e6ea455c61275615a60857
fusspawn <ariochofmelnibone@gmail.com>
2012-01-24 21:55:43 +0000

    sorting out static importer

--- scripts/typeidinstaller.js
+++ scripts/typeidinstaller.js
@@ -5,12 +5,12 @@ var lazy = require("lazy");
 script.run = function(req, res, redis_client) 
 {
     console.log("file loading: " + "../tmp_data/typeids.txt");
-    var ll = new lazy(fs.createReadStream('typeids.txt'))
+    var ll = new lazy(fs.createReadStream('./scripts/typeids.txt'))
     ll.lines.forEach(function(line){
                  var parts = line.split(/\s\s*/g);
                  var id = parts.shift();
                  var name = parts.join(" ");
                  redis_client.set("ccp.static.type_ids."+id, name);
                  res.write("saving: " + id);
-    });
+    }); //
 };

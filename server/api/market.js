7554485a8b03208bacae76c9afe6f576f4183a80
fusspawn <ariochofmelnibone@gmail.com>
2012-01-24 22:37:52 +0000

    csv fix

--- server/api/market.js
+++ server/api/market.js
@@ -3,7 +3,7 @@ var csv = require("ya-csv");
 
 
 market_api.market_upload = function(data, redis_client) {
-    var csv_parser  = csv.createCsvFileReader({ columnsFromHeader: true });
+    var csv_parser  = csv.createCsvStreamReader({ columnsFromHeader: true });
     csv_parser.addListener('data', function(data) {
         redis_client.set("ccp.dynamic.market_order."+data.orderID, JSON.stringify(data));
         console.log("we just saved orderID: " +data.orderID+ " to redis :D");

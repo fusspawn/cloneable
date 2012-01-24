d3d0bdb5f634ce1655260bf6bc164664a7d48e83
fusspawn <ariochofmelnibone@gmail.com>
2012-01-24 22:55:10 +0000

    Added Basic Market Display

--- server/api/market.js
+++ server/api/market.js
@@ -5,12 +5,42 @@ var csv = require("ya-csv");
 market_api.market_upload = function(data, redis_client) {
     var csv_parser  = csv.createCsvStreamReader({ columnsFromHeader: true });
     csv_parser.addListener('data', function(data) {
-        redis_client.set("ccp.dynamic.market_order."+data.orderID, JSON.stringify(data));
+        redis_client.set("ccp.dynamic.market_order."+data.orderID, JSON.stringify(data)); //ccp.dynamic.market_order.2417444789
         console.log("we just saved orderID: " +data.orderID+ " to redis :D");
     });
+    
     csv_parser.parse(data);
 };
 
 
 
-market_api.market_history_upload = function(data, redis_client) {};
+market_api.market_history_upload = function(data, redis_client) {
+    var csv_parser  = csv.createCsvStreamReader({ columnsFromHeader: true });
+    csv_parser.addListener('data', function(data) {
+        redis_client.set("ccp.dynamic.market_order_history."+data.orderID, JSON.stringify(data)); //ccp.dynamic.market_order.2417444789
+        console.log("we just saved orderID: " +data.orderID+ " to redis :D");
+    }); 
+    csv_parser.parse(data);
+};
+
+market_api.get_order = function(orderid, redis_client, callback) {
+    redis_client.get("ccp.dynamic.market_order."+orderid, function(err, reply) {
+        if(err) {
+            console.log("unable to find order_id: "+orderid);
+            callback("unable to find order_id: "+orderid, null);
+        }
+        
+        callback(null, JSON.parse(reply));
+    });
+};
+
+market_api.get_known_orders = function(redis_client, callback) {
+    redis_client.keys("ccp.dynamic.market_order.*", function(err, reply) {
+        if(err) {
+            console.log("err: unable to list orders: " + err);
+            callback("err: unable to list orders: " + err, null);
+        }
+        
+        return reply;
+    });
+};
--- server/register_handlers.js
+++ server/register_handlers.js
@@ -4,15 +4,20 @@ var market_api = require("./api/market.js");
 handlers.register_handlers = function(app, redis_client)
 {
         app.post("/api/post/markethistory", function(req, res) {
-            console.log("history hit with: " + req.param("data", "empty"));
             market_api.market_history_upload(req.param("data", null), redis_client);
             res.end("History Hit");
         });
     
         app.post("/api/post/market", function(req, res) {
-            console.log("market hit with: "  + req.param("data", "empty"));
             market_api.market_upload(req.param("data", null), redis_client);
             res.end("Market Hit");
+        });
+        
+        
+        app.get("/market/display_known", function(req, res) {
+            market_api.get_known_orders(function(err, data) {
+                res.send(JSON.stringify(data));    
+            });
         });
         
         console.log("registered handlers");

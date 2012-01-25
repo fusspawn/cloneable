f61968c15f5775ad420d9d61a415d815c1bfd6d8
fusspawn <ariochofmelnibone@gmail.com>
2012-01-25 00:39:21 +0000

    Added Order Display View

--- server/register_handlers.js
+++ server/register_handlers.js
@@ -6,8 +6,7 @@ handlers.register_handlers = function(app, redis_client)
         app.set('view options', {
           layout: false
         });
-    
-    
+
         app.post("/api/post/markethistory", function(req, res) {
             market_api.market_history_upload(req.param("data", null), redis_client);
             res.end("History Hit");
@@ -21,8 +20,22 @@ handlers.register_handlers = function(app, redis_client)
         
         app.get("/market/display_known", function(req, res) {
             market_api.get_known_orders(redis_client, function(err, data) {
-                res.render("knownorders.ejs", {orders: data});  //  
+                if(err) {
+                    res.render("fuck.ejs", {error_message: err});
+                    return;
+                }
+                res.render("knownorders.ejs", {orders: data, date: new Date().toString()});  //  
             });
+        });
+        
+        app.get("/market/display_order", function(req, res) {
+            market_api.get_order(redis_client, function(err, data) {
+                if(err) {
+                    res.render("fuck.ejs", {error_message: err});
+                    return;
+                }
+                res.render("displayorder.ejs", {order: data, date: new Date().toString()});  
+            });    
         });
         
         console.log("registered handlers");

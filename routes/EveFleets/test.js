a65b860fda4b586d789a4ec86942ae252d71fa43
Paul Thornton <ariochofmelnibone@gmail.com>
2012-01-30 22:00:19 +0000

    [Fixed]Added shipsave logic / handler

--- routes/EveFleets/test.js
+++ routes/EveFleets/test.js
@@ -9,11 +9,11 @@ app.post("/ships/admin/create_new", function(req, res) {
     Ship.save(function(err) {
         if(err) {
             console.log(err);
-            res.render("ship/create.ejs", {error_message: err});
+            res.render("ship/create.ejs", {error_message: err, success_message: null});
             return;
         }    
         
         console.log("saved new shipbase to mongo");
-        res.render("ship/create.ejs", {success_message: "Awesome, Saved!"});
+        res.render("ship/create.ejs", {error_message: null, success_message: "Awesome, Saved!"}});
     });
 });

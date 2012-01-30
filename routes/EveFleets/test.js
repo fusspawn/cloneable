4f0026ba0b6e085fb90664c84ac9d1d102e36eca
Paul Thornton <ariochofmelnibone@gmail.com>
2012-01-30 21:52:47 +0000

    Added shipsave logic / handler

--- routes/EveFleets/test.js
+++ routes/EveFleets/test.js
@@ -1,4 +1,20 @@
 app.get("/ships/admin/new", function(req, res) {
     console.log("/ships/admin/new request received");
-    res.render("ship/create");
+    res.render("ship/create.ejs");
+});
+
+app.post("/ships/admin/create_new", function(req, res) {
+    
+    var Ship = new mongoose.model("ShipBase");
+    Ship.ship_name = req.param("ship_name", "NameNotFound");
+    Ship.save(function(err) {
+        if(err) {
+            console.log(err);
+            res.render("ship/create.ejs", {error_message: err});
+            return;
+        }    
+        
+        console.log("saved new shipbase to mongo");
+        res.render("ship/create.ejs", {success_message: "Awesome, Saved!"});
+    });
 });

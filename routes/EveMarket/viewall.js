a724c5ec1aab73e8b0e535e993040dfb4f9135d0
Paul Thornton <ariochofmelnibone@gmail.com>
2012-01-31 22:51:29 +0000

    .

--- routes/EveMarket/viewall.js
+++ routes/EveMarket/viewall.js
@@ -2,7 +2,16 @@ var mongoose = require("mongoose");
 app.get("/market/orders/view/all", function(req, res) {
     var model = mongoose.model("market_order");
     console.log("pulling all the mongo docs from the store <3");
-    model.find({}, function (err, docs) {
+    var query = model.find({});
+    console.log("exec'ed find all query");
+    query.limit(25);
+    console.log("limit to 25 items");
+    
+    console.log(query);
+    console.log("exec query");
+    
+    query.exec( function (err, docs) {
+        console.log("query response");
         if(err) {
             console.log(err);
             res.send("something went wrong..");
@@ -12,5 +21,5 @@ app.get("/market/orders/view/all", function(req, res) {
         console.log("all the docs loaded aok. rendering");
         res.render("./market/showall.ejs", {orders: docs});
         console.log("page rendered.");
-    });
+    );
 });

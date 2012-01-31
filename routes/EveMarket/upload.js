83da42ebba67dd7899a9cb8ad81a25551b741e4e
Paul Thornton <ariochofmelnibone@gmail.com>
2012-01-31 21:44:30 +0000

    .

--- routes/EveMarket/upload.js
+++ routes/EveMarket/upload.js
@@ -1,11 +1,16 @@
 var csv = require("ya-csv");
+var mongoose = require("mongoose");
 
 app.post("/api/post/market", function(req, res) {
+    console.log("got market upload request");
     var csv_string = req.param("data", null);
     var csv_parser  = csv.createCsvStreamReader({ columnsFromHeader: true });
     csv_parser.addListener('data', function(data) {
+        console.log("market csv item parsed");
         var order = new mongoose.model("market_order");
         merge(order, data);
+        console.log("merged item to mongoose model");
+        console.log("saving");
         order.save(function(err) {
             if(err) {
                 console.log(err);
@@ -18,6 +23,7 @@ app.post("/api/post/market", function(req, res) {
         });
     });
     
+    console.log("initing parser");
     csv_parser.parse(csv_string);
 });
     

fbc52a54b21de06d491096c9b6dc7f4ba1278691
Paul Thornton <ariochofmelnibone@gmail.com>
2012-01-31 23:12:11 +0000

    more loggings.

--- routes/EveMarket/viewall.js
+++ routes/EveMarket/viewall.js
@@ -2,9 +2,6 @@ var mongoose = require("mongoose");
 var mongoose_url = env["DOTCLOUD_MONGO_MONGODB_URL"];
 console.log("Connection Mongo to: " + mongoose_url);
 mongoose.createConnection(mongoose_url);
-mongoose.on("error", function(errorObject){
-  console.log(errorObject);
-});
 
 app.get("/market/orders/view/all", function(req, res) {
     var model = mongoose.model("market_order");

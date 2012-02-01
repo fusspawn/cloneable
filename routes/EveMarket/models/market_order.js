27006f35b2cd2b0a6318435d9648eaf045e1e975
Paul Thornton <ariochofmelnibone@gmail.com>
2012-02-01 00:31:50 +0000

    .

--- routes/EveMarket/models/market_order.js
+++ routes/EveMarket/models/market_order.js
@@ -1,18 +1,9 @@
 mongoose.model("market_order", new mongoose.Schema({
     price           : Number,
     volRemaining    : Number,
-    typeID          : {type: Number,
-                        get: function(typeID) {
-                             console.log("doing redis_lookup on: "+typeID);
-                             redis_client.get("ccp.static.type_ids."+typeID, function(err, reply) { 
-                                if(err) return typeID;
-                                console.log("did redis_lookup on: "+typeID + " returned: "+reply);
-                                return reply;
-                             });
-                        }
-                      },
+    typeID          : {type: Number },
     range           : Number,
-    orderID         : Number,
+    orderID         : {type:Number, unique: true},
     volEntered      : Number,
     minVolume       : Number, 
     bid             : Boolean,

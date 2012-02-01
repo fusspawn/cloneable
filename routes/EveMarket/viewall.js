ddb3ff6a4228b7efbe874e64cb324248a236a95a
Paul Thornton <ariochofmelnibone@gmail.com>
2012-02-01 21:01:23 +0000

    async and typenaming

--- routes/EveMarket/viewall.js
+++ routes/EveMarket/viewall.js
@@ -1,3 +1,5 @@
+var $a = require("async");
+
 app.get("/market/orders/view/all", function(req, res) {
     var model = mongoose.model("market_order");
     console.log("pulling all the mongo docs from the store <3");
@@ -9,8 +11,38 @@ app.get("/market/orders/view/all", function(req, res) {
             return;
         }
         
-        console.log("all the docs loaded aok. rendering");
-        res.render("./market/showall.ejs", {orders: docs});
-        console.log("page rendered.");
+        $a.series(
+            function() {
+                console.log("redis lookup on all orderID's");
+                $a.map(docs, function(item, callback) {
+                    redis_client.get("ccp.static.type_ids."+item.typeID, function(err, response) {
+                        callback(err, response);
+                    });
+                }, function(err, result) {
+                    if(err) {console.log(err); return;}
+                    for(i in docs) {
+                        docs[i].real_name = result[i];
+                    }
+                });
+            },
+            function() {
+                console.log("all the docs loaded aok. rendering");
+                res.render("./market/showall.ejs", {orders: docs});
+                console.log("page rendered.");     
+            }
+        );        
     });
 });
+
+
+function(res, orders) {
+     console.log("all the docs loaded aok. rendering");
+     res.render("./market/showall.ejs", {orders: docs});
+     console.log("page rendered.");    
+}
+
+$a.series(
+    function(docs) {},
+    function(res, orders) {}
+);
+

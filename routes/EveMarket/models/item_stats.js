527d3d3c02f72d7b15f2036c15bafd4b34010749
Paul Thornton <ariochofmelnibone@gmail.com>
2012-02-02 23:17:20 +0000

    Schema Fixup

--- routes/EveMarket/models/item_stats.js
+++ routes/EveMarket/models/item_stats.js
@@ -2,8 +2,8 @@ mongoose.model("item_stat", new mongoose.Schema({
    highest_buy: Number,
    lowest_sell: Number,
    typeID: Number,
-   highest_buy_order_id: mongoose.schema.ObjectID,
-   lowest_sell_order_id: mongoose.schema.ObjectID
+   highest_buy_order_id: mongoose.Schema.ObjectID,
+   lowest_sell_order_id: mongoose.Schema.ObjectID
 }));
 
 console.log("loaded item_stat model in mongoose");

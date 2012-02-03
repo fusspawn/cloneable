9c7b504e0ee2a24df91470cec6aeaeef532666ee
Paul Thornton <ariochofmelnibone@gmail.com>
2012-02-03 02:30:46 +0000

    sensible defaults for item_stat

--- routes/EveMarket/models/item_stats.js
+++ routes/EveMarket/models/item_stats.js
@@ -1,11 +1,9 @@
-var order_collection = mongoose.model("market_order");
-
 mongoose.model("item_stat", new mongoose.Schema({
-   highest_buy: Number,
-   lowest_sell: Number,
+   highest_buy: {type: Number, default: 0},
+   lowest_sell: {type: Number, default: 0},
    typeID: Number,
-   highest_buy_order_id: String,
-   lowest_sell_order_id: String
+   highest_buy_order_id: {type: String, default: ""},
+   lowest_sell_order_id: {type: String, default: ""}
 }));
 
 console.log("loaded item_stat model in mongoose");

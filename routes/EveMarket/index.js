515ed3942b9bbf650cd10e6c79d2ad2dcd0faa84
Paul Thornton <ariochofmelnibone@gmail.com>
2012-02-03 00:23:32 +0000

    actually run the update handler

--- routes/EveMarket/index.js
+++ routes/EveMarket/index.js
@@ -1,7 +1,7 @@
 require("./models");
 require("./upload.js");
 require("./viewall.js");
-require("./new_order_handler.js");
+new_order_handler = require("./new_order_handler.js");
 
 app.get("/market/", function(req, res) {
     res.render("./market/index.ejs");
--- routes/EveMarket/new_order_handler.js
+++ routes/EveMarket/new_order_handler.js
@@ -1,15 +1,9 @@
 var $a = require("async");
 var orders_collection = mongoose.model("market_order");
 var item_stats_collection = mongoose.model("item_stat");
+var new_order_handler = module.exports = {};
 
-redis_client.on("subscribe", function(channel, message) {
-    console.log("got pub/sub on " + channel);
-    if(!(channel == "new_market_order"))
-        return;
-        
-    console.log("handling a " + channel + " update");
-    
-    var orderID = JSON.parse(message).id;
+new_order_handler.update_stats = function(orderID) {
     orders_collection.findById(orderID, function(err, order) {
         if(err) {console.log(err); return;}
         var type_id = order.typeID;
@@ -39,15 +33,13 @@ redis_client.on("subscribe", function(channel, message) {
             
             item_stats_collection.findOne({typeID: type_id}, function(err, data) {
                 if(data == null) {
-                    console.log(err); // CreateNew
-                    return;
-                    
                     var new_item_stat = new item_stats_collection();
                     new_item_stat.highest_buy = buy_highest;
                     new_item_stat.highest_buy_order_id = buy_id;
                     new_item_stat.lowest_sell = sell_lowest;
                     new_item_stat.lowest_sell_order_id = sell_id;
                     new_item_stat.save(function(err) { if(err) console.log(err); });
+                    return;
                 }
                     //Update
                     data.highest_buy = buy_highest;

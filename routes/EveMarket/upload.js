1bd3c9242024a069952dc52ebcd7d14b6fb74601
Paul Thornton <ariochofmelnibone@gmail.com>
2012-02-03 00:47:47 +0000

    finally blocks

--- routes/EveMarket/upload.js
+++ routes/EveMarket/upload.js
@@ -1,6 +1,6 @@
 var csv = require("ya-csv");
 var market_order = mongoose.model("market_order");
-
+var item_stats_collection = mongoose.model("item_stat");
 
 //
 app.post("/api/post/market", function(req, res) {
@@ -38,7 +38,7 @@ app.post("/api/post/market", function(req, res) {
                         }   
                         
                         console.log("Saved order, updating redis lastorders");
-                        new_order_handler.update_stats(order._id);
+                        update_stats(order._id);
                     }); 
             } else {
                     order.price  = data.price;
@@ -66,7 +66,7 @@ app.post("/api/post/market", function(req, res) {
                         
                         console.log("Saved order, updating redis lastorders");
                         console.log("handing new order");
-                        new_order_handler.update_stats(order._id);
+                        update_stats(order._id);
                         console.log("new order handled");
                     });
             }
@@ -83,19 +83,51 @@ app.post("/api/post/markethistory", function(req, res) {
     var csv_string = req.param("data", null);       
 });
 
-function merge(obj, json) {
-    obj.price  = json.price;
-    obj.volRemaining = json.volRemaining;
-    obj.typeID  = json.typeID;
-    obj.range   = json.range;
-    obj.orderID = json.orderID;
-    obj.volEntered = json.volEntered;
-    obj.minVolume = json.minVolume;
-    obj.bid = json.bid;
-    obj.issued  = json.issues;
-    obj.duration = json.duration;
-    obj.stationID  = json.stationID;
-    obj.regionID = json.regionID;
-    obj.solarSystemID  = json.solarSystemID = json.solarSystemID;
-    obj.jumps  = json.jumps;
-}
+function update_stats(orderID) {
+    market_order.findById(orderID, function(err, order) {
+        if(err) {console.log(err); return;}
+        var type_id = order.typeID;
+        console.log("doing a recalc on typeID: " + type_id);
+        market_order.find({typeID: type_id}, function(err, docs) {
+            if(err) {console.log(err); return;} 
+            var buy_highest = 0;
+            var sell_lowest = 99999999999;
+            var buy_id;
+            var sell_id;
+            
+            docs.forEach(function(doc) {
+                if(doc.bid) {
+                    if(doc.price > buy_highest) {
+                        buy_highest = doc.price;
+                        buy_id = doc._id;
+                    }
+                }
+                else {
+                    if(doc.price < sell_lowest) {
+                        sell_lowest = doc.price;
+                        sell_id = doc._id;
+                    }
+                }
+            });
+            
+            
+            item_stats_collection.findOne({typeID: type_id}, function(err, data) {
+                if(data == null) {
+                    var new_item_stat = new item_stats_collection();
+                    new_item_stat.highest_buy = buy_highest;
+                    new_item_stat.highest_buy_order_id = buy_id;
+                    new_item_stat.lowest_sell = sell_lowest;
+                    new_item_stat.lowest_sell_order_id = sell_id;
+                    new_item_stat.save(function(err) { if(err) console.log(err); });
+                    return;
+                }
+                    //Update
+                    data.highest_buy = buy_highest;
+                    data.highest_buy_order_id = buy_id;
+                    data.lowest_sell = sell_lowest;
+                    data.lowest_sell_order_id = sell_id;
+                    data.save(function(err) { if(err) console.log(err); });
+            });
+        });
+    });
+});

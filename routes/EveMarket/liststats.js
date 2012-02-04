994b10ab523b4a82a55cfaa65346309605e21c96
Paul Thornton <ariochofmelnibone@gmail.com>
2012-02-04 00:15:11 +0000

    init

--- routes/EveMarket/liststats.js
+++ routes/EveMarket/liststats.js
@@ -3,10 +3,7 @@ var $a = require("async");
 
 app.get("/market/stats/view", function(req, res) {
     $stats.find({}, function(err, docs) {
-         if(!req.headers['HTTP_EVE_STATIONID'])
-            res.send("please authorise this site and reload");
-         
-         var stationID = req.headers['HTTP_EVE_STATIONID'];
+         var stationID = req.param("station_id", 0);
          $stats.find({station_id: stationID}, function(err, docs) {
              if(err) {
                 console.log(err);

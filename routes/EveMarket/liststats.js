84a3089d14ccfa448cb5573e84727cf88fcbf6e2
Paul Thornton <ariochofmelnibone@gmail.com>
2012-02-04 00:06:50 +0000

    update stats rendering to new very much simpler more awesomesauceness

--- routes/EveMarket/liststats.js
+++ routes/EveMarket/liststats.js
@@ -3,27 +3,19 @@ var $a = require("async");
 
 app.get("/market/stats/view", function(req, res) {
     $stats.find({}, function(err, docs) {
-         $a.map(docs, 
-                function(item, callback) {
-                    redis_client.get("ccp.static.type_ids."+item.typeID, function(err, response) {
-                        callback(err, response);
-                    });
-                },function(err, result) {
-                    if(err) {console.log(err); return;}
-                    for(var i in docs) {
-                        docs[i].item_name = result[i];
-                        if(docs[i].highest_buy == 0 || docs[i].lowest_sell == 0 || docs[i].lowest_sell == 99999999999 || docs[i].highest_buy == 0)
-                            docs[i].profit = 0;
-                        else {
-                            docs[i].profit = docs[i].lowest_sell - docs[i].highest_buy;
-                            if(docs[i].profit > 50000000) {
-                                docs[i].profit = 0;
-                                docs[i].item_name = docs[i].item + "(Customised - Order)";
-                            }
-                        }
-                    }
-                    res.render("./market/liststats.ejs", {stats: docs});
-         });         
+         if(!req.headers['HTTP_EVE_STATIONID'])
+            res.send("please authorise this site and reload");
+         
+         var stationID = req.headers['HTTP_EVE_STATIONID'];
+         $stats.find({station_id: stationID}, function(err, docs) {
+             if(err) {
+                console.log(err);
+                res.send(err);
+                return;
+             }
+             
+             res.render("./market/liststats.ejs", {stats: docs});
+         });
     });
 });
 
